3694770720907807f4d56a4ed438c79fcb4a5be9
gonczor <wiktor.gonczaronek@gmail.com>
2016-02-10 18:27:10 +0100

    Main changed to objet-oriented, setup functions created, but not tested yet

--- PyHomeServer.py
+++ PyHomeServer.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
 
-from Networking import ConnectionHandler
-from ReadSetup import get_setup
 from Interface import Output
+from Networking import ConnectionHandler
+from Setup.ReadSetup import get_setup
 
+
+class Main:
+    def __init__(self):
+        self.__connection__ = None
+
+    def main(self):
+        try:
+            self.__connection_thread__()
+        except FileNotFoundError:
+            Output.config_error()
+        except KeyboardInterrupt:
+            print('Ending')
+        finally:
+            self.__connection__.shutdown()
+
+    # Function will be edited in final version so that restarting server will not be necessary.
+    # Currently easier version is used.
+    def __connection_thread__(self):
+            host, port = get_setup()
+            self.__connection__ = ConnectionHandler.setup_connection_handler(host, port)
+            self.__connection__.serve_forever()
+
+    def __setup_thread__(self):
+        pass
+
+    def __SPI_thread(self):
+        pass
 
 if __name__ == '__main__':
-    host, port = get_setup()
-    connection = ConnectionHandler.setup_connection_handler(host, port)
-    try:
-        connection.serve_forever()
-    except FileNotFoundError:
-        Output.config_error()
-    except KeyboardInterrupt:
-        print('Ending')
-    finally:
-        connection.shutdown()
+    main = Main()
+    main.main()
+
---
+++ Setup/SetupHandler.py
@@ -0,0 +1,20 @@
+import socket
+
+
+setup_handler_port = 12346
+setup_handler_address = '127.0.0.1'
+
+
+def setup_handler():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((setup_handler_address, setup_handler_port))
+    s.listen(1)
+
+    # Data needs to be transmitted from local host in order to prevent someone from outside
+    # modifying the program flow
+    data_from_local_host_received = False
+    while not data_from_local_host_received:
+        conn, addr = s.accept()
+        if addr[0] == '127.0.0.1':
+            data_from_local_host_received = True
+
