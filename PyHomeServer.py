532dfe205a89f6eff059b0ccc0c72c31b4fb76bc
gonczor <wiktor.gonczaronek@gmail.com>
2016-02-24 23:51:59 +0100

    prepared for splitting tasks into threads

--- PyHomeServer.py
+++ PyHomeServer.py
@@ -3,34 +3,72 @@
 from Interface import Output
 from Networking import ConnectionHandler
 from Setup.ReadSetup import get_setup
+import threading
 
 
-class Main:
+class Threads(threading.Thread):
+    def __init__(self, service):
+        threading.Thread.__init__(self)
+        self.service = service
+
+    def run(self):
+        self.service.service_entry_point()
+
+
+class Services:
+    # Entry point for threads
+    def service_entry_point(self):
+        pass
+
+
+class ClientCommunicationService(Services):
     def __init__(self):
         self.__connection__ = None
 
-    def main(self):
+    def service_entry_point(self):
         try:
-            self.__connection_thread__()
+            self.__connect__()
         except FileNotFoundError:
             Output.config_error()
-        except KeyboardInterrupt:
-            print('Ending')
         finally:
             self.__connection__.shutdown()
 
     # Function will be edited in final version so that restarting server will not be necessary.
     # Currently easier version is used.
-    def __connection_thread__(self):
+    def __connect__(self):
             host, port = get_setup()
             self.__connection__ = ConnectionHandler.setup_connection_handler(host, port)
             self.__connection__.serve_forever()
 
-    def __setup_thread__(self):
+
+class AdminService(Services):
+    def service_entry_point(self):
         pass
 
-    def __SPI_thread(self):
+
+class EmbeddedCommunicationService(Services):
+    def service_entry_point(self):
         pass
+
+
+class Main:
+    @staticmethod
+    def main():
+        client_communication_service = ClientCommunicationService()
+        admin_service = AdminService()
+        embedded_communication_service = EmbeddedCommunicationService()
+
+        client_communication_service_thread = Threads(client_communication_service)
+        admin_service_thread = Threads(admin_service)
+        embedded_communication_service_thread = Threads(embedded_communication_service)
+
+        client_communication_service_thread.start()
+        admin_service_thread.start()
+        embedded_communication_service_thread.start()
+
+        client_communication_service_thread.join()
+        admin_service_thread.join()
+        embedded_communication_service_thread.join()
 
 if __name__ == '__main__':
     main = Main()
