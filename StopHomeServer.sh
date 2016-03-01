090c8cf99a86bb6605748f6896fa4fad0a340f75
gonczor <wiktor.gonczaronek@gmail.com>
2016-03-01 20:19:14 +0100

    Corrected bash scripts to react better while server is not working.

--- StopHomeServer.sh
+++ StopHomeServer.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 SERVER_PID=$(ps -ax | grep PyHomeServer.py | grep -v grep | awk '{print $1}')
-kill -s SIGINT $SERVER_PID
+if [ "$SERVER_PID" = "" ]; then
+    echo "No Server process is running. Type ./PyHomeServer.py to run server."
+else
+    kill -s SIGINT $SERVER_PID
+    echo "Waiting for server to stop..."
+    sleep 1s
+    SERVER_PID=$(ps -ax | grep PyHomeServer.py | grep -v grep | awk '{print $1}')
+    if [ "$SERVER_PID" = "" ]; then
+        echo "Server has been stopped successfully"
+    fi
+fi
