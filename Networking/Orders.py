d797293b742a93cf7acc0b542d1b329cd6e04998
gonczor <wiktor.gonczaronek@gmail.com>
2016-03-11 13:35:34 +0100

    Added abstract function for dealing with orders

--- Networking/Orders.py
+++ Networking/Orders.py
@@ -2,12 +2,21 @@ class Order:
     def get_message(self):
         pass
 
+    def perform_order(self):
+        pass
+
 
 class Welcome(Order):
     def get_message(self):
         return 'welcome'
 
+    def perform_order(self):
+        pass
+
 
 class SendFile(Order):
     def get_message(self):
         return 'send_file'
+
+    def perform_order(self):
+        pass
