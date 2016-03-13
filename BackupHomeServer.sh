561c5e26511ceae7e9ef59c0d72491be246cdb22
gonczor <wiktor.gonczaronek@gmail.com>
2016-03-13 09:05:49 +0000

    Preparations for new branch

--- BackupHomeServer.sh
+++ BackupHomeServer.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# check if the baskup is already set up
-BACKUP_SET=$(cat /etc/crontab | grep BackupHomeServer.sh | grep - v grep)
 
-if [ $BACKUP_SET="" ]; then
-	echo "Brak backupu"
-else
-	echo "Jest backup"
-fi
