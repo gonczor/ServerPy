86d1136dc813bf4abff9c62aa644b36c956c1007
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-07 23:11:44 +0200

    Added stop script to main

--- server.sh
+++ server.sh
@@ -3,21 +3,14 @@
 # This script is used for launching server and configuration scripts from Admin/ directory.
 # See Admin/help or README.md for further details.
 
-#   * no args - start
-#   * start
-#   * config
-#   * help
-#   * backup - run backup
-#       - set
-#       - retrieve
-#   * help
-
 if [ "$#" = 0 ] || [ "$1" = "start" ]; then
     bash Admin/Start.sh
 elif [ "$1" = "config" ]; then
     bash Admin/ConfigHomeServer.sh
 elif [ "$1" = "help" ]; then
     cat Admin/help
+elif [ "$1" = "stop" ]; then
+    bash Admin/StopHomeServer.sh
 elif [ "$1" = "backup" ]; then
     if [ "$2" = "" ]; then
         bash Admin/BackupHomeServer.sh
