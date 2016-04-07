a6d876162d9c3fcc953a175b12d24b1618c8c42c
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-07 22:18:52 +0200

    Corrected Start script

--- Admin/Start.sh
+++ Admin/Start.sh
@@ -3,7 +3,8 @@
 SERVER_PID=$(ps -ax | grep PyHomeServer.py | grep -v grep | awk '{print $1}')
 
 if [ "$SERVER_PID" = "" ]; then
-    ../PyHomeServer.py
+    printf "Starting server..."
+    python3 ./PyHomeServer.py
 else
     printf "Server is already running. Process ID is: $SERVER_PID"
 fi
