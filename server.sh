8e5fc4499046b11afae8039ddc7bba86e84cf74f
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-07 22:34:54 +0200

    Completed main script.

--- server.sh
+++ server.sh
@@ -12,6 +12,18 @@
 #       - retrieve
 #   * help
 
-if [ "$#" = 0 ]; then
+if [ "$#" = 0 ] || [ "$1" = "start" ]; then
     bash Admin/Start.sh
+elif [ "$1" = "config" ]; then
+    bash Admin/ConfigHomeServer.sh
+elif [ "$1" = "help" ]; then
+    cat Admin/help
+elif [ "$1" = "backup" ]; then
+    if [ "$2" = "" ]; then
+        bash Admin/BackupHomeServer.sh
+    elif [ "$2" = "retrieve" ]; then
+        bash Admin/BackupHomeServer.sh $2
+    fi
+else
+    echo "Unknown command. Run script with \"help\" to display help."
 fi
