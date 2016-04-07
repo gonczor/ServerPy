978edbf8355e5fafea471010d7f78a87334211f4
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-07 22:20:03 +0200

    Basic functionality in main script

--- server.sh
+++ server.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
 # This script is used for launching server and configuration scripts from Admin/ directory.
-# See Admin/help or README.md for further detail
+# See Admin/help or README.md for further details.
+
+#   * no args - start
+#   * start
+#   * config
+#   * help
+#   * backup - run backup
+#       - set
+#       - retrieve
+#   * help
+
+if [ "$#" = 0 ]; then
+    bash Admin/Start.sh
+fi
