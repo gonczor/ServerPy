401cfbabdd79cf2211f427bb2ee6b181d2338817
gonczor <wiktor.gonczaronek@gmial.com>
2016-04-23 15:46:00 +0200

    Tries to make backup script work v1

--- Admin/BackupHomeServer.sh
+++ Admin/BackupHomeServer.sh
@@ -1,30 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+
+WORKING_DIRECTORY="$HOME/Documents/ServerPy"
 
 function set_backup(){
-    SCHEDULE_FILE="Configuration/crontab"
+    SCHEDULE_FILE="$WORKING_DIRECTORY/Configuration/crontab"
     SCHEDULE="* * * * *"
     USER=$(whoami)
-    WORKING_DIRECTORY="$PWD"
-    SCRIPT_NAME="/Admin/BackupHomeServer.sh"
-    COMMAND=' /bin/bash ' #these spaces are essential for proper file formatting
+    SCRIPT_NAME="$0"
+    COMMAND='/bin/bash' #these spaces are essential for proper file formatting
     printf "$SCHEDULE " > $SCHEDULE_FILE
-    #printf "$USER" >> $SCHEDULE_FILE
-    printf "$COMMAND" >> $SCHEDULE_FILE
-    printf "$WORKING_DIRECTORY" >> $SCHEDULE_FILE
-    printf "$SCRIPT_NAME\n" >> $SCHEDULE_FILE
-    crontab $SCHEDULE_FILE
+    printf " $USER " >> $SCHEDULE_FILE
+    printf " $COMMAND " >> $SCHEDULE_FILE
+    printf " $SCRIPT_NAME\n" >> $SCHEDULE_FILE
+    crontab "$SCHEDULE_FILE"
 }
 
 function perform_backup(){
     BACKUP_FILENAME=$(date +%Y-%m-%d)
     BACKUP_FILENAME+=".tar.gz"
-    BACKUP_DIR=$(cat Configuration/PyHomeServer.conf | grep "Backup directory:" |  awk '{print $3}')
+    BACKUP_DIR="$WORKING_DIRECTORY/"
+    BACKUP_DIR+="$(cat $WORKING_DIRECTORY/Configuration/PyHomeServer.conf | grep "Backup directory:" |  awk '{print $3}')"
 
-    FILES_TO_INCLUDE="Configuration/ Database/"
+    echo "$BACKUP_DIR"
+    FILES_TO_INCLUDE="$WORKING_DIRECTORY/Configuration/ $WORKING_DIRECTORY/Database/"
+    echo Files to include
+    echo "$FILES_TO_INCLUDE"
     if [ ! -d "$BACKUP_DIR" ]; then
-        mkdir $BACKUP_DIR
+        mkdir "$BACKUP_DIR"
     fi
-    tar -cvzpf $BACKUP_DIR/$BACKUP_FILENAME $FILES_TO_INCLUDE
+	echo "I'm before taring"
+    tar cvzpf "$BACKUP_DIR/$BACKUP_FILENAME" "$FILES_TO_INCLUDE"
+	echo "And after"
 }
 
 
@@ -32,7 +38,7 @@ function retrieve_latest_backup(){
     echo To be implemented
 }
 
-if [ "$#" = "0" ]; then
+if [ $# -eq 0 ]; then
     perform_backup
 elif [ "$1" = "retrieve" ]; then
     retrieve_latest_backup
--- server.sh
+++ server.sh
@@ -3,16 +3,18 @@
 # This script is used for launching server and configuration scripts from Admin/ directory.
 # See Admin/help or README.md for further details.
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ "$#" = 0 ] || [ "$1" = "start" ]; then
-    bash Admin/Start.sh
+    bash "$DIR/Admin/Start.sh"
 elif [ "$1" = "config" ]; then
-    bash Admin/ConfigHomeServer.sh
+    bash "$DIR/Admin/ConfigHomeServer.sh"
 elif [ "$1" = "help" ]; then
-    cat Admin/help
+    cat "$DIR/Admin/help"
 elif [ "$1" = "stop" ]; then
-    bash Admin/StopHomeServer.sh
+    bash "$DIR/Admin/StopHomeServer.sh"
 elif [ "$1" = "backup" ]; then
-    bash Admin/BackupHomeServer.sh $2
+    bash $DIR/Admin/BackupHomeServer.sh $2
 else
     echo "Unknown command. Run script with \"help\" to display help."
 fi
