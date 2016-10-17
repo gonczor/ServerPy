9fe75aa9d1f425d360bd89d5019d88e0eb9c217f
gonczor <wiktor.gonczaronek@gmail.com>
2016-10-18 00:35:36 +0200

    extended main script

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # This script is used for launching server and configuration scripts from Admin/ directory.
 # See Admin/help or README.md for further details.
@@ -14,6 +14,8 @@ elif [ "$1" = "stop" ]; then
     bash "Admin/StopHomeServer.sh"
 elif [ "$1" = "backup" ]; then
     bash Admin/BackupHomeServer.sh $2
+elif [ "$1" = "ssl" ]; then
+    bash Admin/ssl.sh $2
 else
     echo "Unknown command. Run script with \"help\" to display help."
 fi
