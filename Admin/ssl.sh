1b4b78f615792721dd65fd8aee72fb0e3ed65a41
gonczor <wiktor.gonczaronek@gmail.com>
2016-10-22 11:14:49 +0200

    Fighting with ssl

---
+++ Admin/ssl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function create_production_ssl_certs(){
+    # TODO implement
+    echo "Not implemented"
+}
+
+
+function create_test_ssl_certs(){
+    # TODO these pathnames suck. Improve.s
+    sudo openssl req -new > new.ssl.csr
+    sudo openssl rsa -in privkey.pem -out new.cert.key
+    sudo openssl x509 -in new.cert.csr -out new.cert.cert -req -signkey new.cert.key -days NNN
+    sudo cp new.cert.cert Configuration/SSL/server.crt
+    sudo cp new.cert.key Configuration/SSL/server/key
+}
+
+if [ $# -eq 0 ]; then
+    create_production_ssl_certs
+elif [ "$1" = "test" ]; then
+    create_test_ssl_certs
+fi
