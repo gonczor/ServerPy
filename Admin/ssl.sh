44323c28f60a90c1d452bcf20fe2bb166dfb822f
gonczor <wiktor.gonczaronek@gmail.com>
2016-10-29 02:11:31 +0200

    Corrected naming issue and added docs

--- Admin/ssl.sh
+++ Admin/ssl.sh
@@ -10,8 +10,8 @@ function create_test_ssl_certs(){
     sudo openssl req -new > new.ssl.csr
     sudo openssl rsa -in privkey.pem -out new.cert.key
     sudo openssl x509 -in new.ssl.csr -out new.cert.cert -req -signkey new.cert.key -days 999
-    sudo cp new.cert.cert Configuration/SSL/
-    sudo cp new.cert.key Configuration/SSL/
+    sudo cp new.cert.cert Configuration/SSL/server.crt
+    sudo cp new.cert.key Configuration/SSL/server.key
     sudo rm new.ssl.csr
     sudo rm new.cert.cert
     sudo rm new.cert.key
