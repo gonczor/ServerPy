b1bc1f3e4389101902cd59929d9ec1e77a493f85
gonczor <wiktor.gonczaronek@gmail.com>
2017-01-04 15:23:25 +0100

    gitignore update

--- simple_client.py
+++ simple_client.py
@@ -6,11 +6,11 @@ BASE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ssl_sock = ssl.wrap_socket(s,
-                           ca_certs=os.path.join(BASE_PATH, 'Configuration', 'SSL', 'server.crt'),
+                           ca_certs=os.path.join(BASE_PATH, 'Configuration', 'SSL', 'server2.crt'),
                            cert_reqs=ssl.CERT_REQUIRED,
                            )
 
-ssl_sock.connect(('127.0.0.1', 12345))
+ssl_sock.connect(('192.168.43.57', 12345))
 
 print(ssl_sock.getpeername())
 print(ssl_sock.cipher())
@@ -19,7 +19,8 @@ print(ssl_sock.cipher())
 ssl_sock.send(b'user pass')
 received = ssl_sock.recv(1024)
 # ssl_sock.write()
-ssl_sock.send(b'welcome')
+data_to_send = input('Give command: ')
+ssl_sock.send(bytearray(data_to_send, 'utf-8'))
 print('Received: {}'.format(received))
 
 data = ssl_sock.read()
