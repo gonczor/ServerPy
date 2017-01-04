deb023819eb3238304da721550f05bb52292ef0c
gonczor <wiktor.gonczaronek@gmail.com>
2017-01-04 15:20:38 +0100

    deleted obsolete import

--- tests/testcases/__init__.py
+++ tests/testcases/__init__.py
@@ -1,6 +1,5 @@
 # This file has to store all .py files containing test cases using following pattern:
 # .TestPackage.TestFile import *
 # all test cases names must start with 'test' to make them run.
-from .NetworkTests.AnotherTests import *
 from .NetworkTests.AuthenticationTests import *
 from .NetworkTests.BannedAdressCacheTests import *
