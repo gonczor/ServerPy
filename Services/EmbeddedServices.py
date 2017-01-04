e251ae6b74a370ef08b2e8aa82f0c8f25931154a
gonczor <wiktor.gonczaronek@gmail.com>
2017-01-04 16:26:43 +0100

    added imports

--- Services/EmbeddedServices.py
+++ Services/EmbeddedServices.py
@@ -1,3 +1,10 @@
+import RPi.GPIO as GPIO
+import spidev
+from lib_nrf24 import NRF24
+
+# Note: for development purposes I use dummy version of RPi.GPIO, spidev and lib_nrf24 on my laptop.
+#  It doesn't do anything apart from providing interface for development without getting import errors.
+
 from Services.Base import Services
 
 
